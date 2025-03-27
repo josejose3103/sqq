cc1999fccfa4f268dc129a1277ff861b567c4bdb
josejose <josejose3103@gmail.com>
2025-03-27 18:01:18 +0900

    １１−２５

--- import.sql
+++ import.sql
@@ -21,3 +21,21 @@ SELECT number, byoumei, kingaku FROM article6 WHERE day BETWEEN '2022-01-11' AND
   SELECT * FROM table4 WHERE personalID = '1514645329' INTO OUTFILE '/tmp/11.csv';;
   SELECT DATE_FORMAT(day, '%Y') as Y, sum(金額) FROM article6 GROUP by DATE_FORMAT(day, '%Y');
   SELECT number as 番号, byoumei as 病名, kingaku as 金額, day as 日付 FROM article3 WHERE day BETWEEN '2022-01-01' AND '2022-01-28' AND kumikanbangou = '22.0';
+  media=# SELECT 
+    CASE 
+        WHEN EXTRACT(DAY FROM day) BETWEEN 11 AND 25 THEN '11-25'
+        ELSE '26-10'
+    END AS period,
+    EXTRACT(YEAR FROM day) AS year,
+    CASE 
+        WHEN EXTRACT(DAY FROM day) BETWEEN 11 AND 25 THEN EXTRACT(MONTH FROM day)
+        ELSE EXTRACT(MONTH FROM day + INTERVAL '1 month')
+    END AS month,
+    SUM(kingaku) AS total_sales
+FROM article3
+WHERE day >= '2024-01-01'  -- 必要に応じて期間を指定
+GROUP BY year, month, period
+ORDER BY year, month, period;
+SELECT to_char(sum(kingaku),'¥9,999,999') as 合計 FROM article3 WHERE day BETWEEN '2025-01-01' AND '2025-01-28' AND kumikanbangou = '22.0';
+    合計     
+media=# SELECT number as 番号, byoumei as 病名, to_char(kingaku, '¥9,999,999') as金額, day as 日付 FROM article3 WHERE day BETWEEN '2022-01-01' AND '2022-01-28' AND kumikanbangou = '22.0';
